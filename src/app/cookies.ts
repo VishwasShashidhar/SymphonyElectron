d053f5541da650fd5599659f14f7194a9b9e52ec
Vishwas Shashidhar <vishwas.shashidhar@symphony.com>
2021-02-17 12:46:50 +0530

    SDA-2647: move cookie handler to a different file

---
+++ src/app/cookies.ts
@@ -0,0 +1,33 @@
+import * as electron from 'electron';
+import { logger } from '../common/logger';
+
+const cookies = electron.session.defaultSession.cookies;
+
+export const handleCookies = (): void => {
+  cookies.on('changed', async (_event, cookie, _cause, removed) => {
+    if (cookie.session && !removed) {
+      const url = `${cookie.secure ? 'https' : 'http'}://${cookie.domain}${
+        cookie.path
+      }`;
+      try {
+        logger.info(
+          `Trying to persist cookie ${JSON.stringify(cookie)} for ${url}`,
+        );
+        await cookies.set({
+          url,
+          name: cookie.name,
+          value: cookie.value,
+          domain: cookie.domain,
+          path: cookie.path,
+          secure: cookie.secure,
+          httpOnly: cookie.httpOnly,
+          expirationDate: Math.floor(new Date().getTime() / 1000) + 1209600,
+        });
+      } catch (e) {
+        logger.error(
+          `Failed to persist cookies due to -> ${JSON.stringify(e)}`,
+        );
+      }
+    }
+  });
+};
--- src/app/window-handler.ts
+++ src/app/window-handler.ts
@@ -9,12 +9,12 @@ import {
   globalShortcut,
   ipcMain,
   screen,
-  session,
   shell,
 } from 'electron';
 import * as fs from 'fs';
 import * as path from 'path';
 import { format, parse } from 'url';
+import * as cookieHandler from './cookies';
 
 import { apiName, Themes, WindowTypes } from '../common/api-interface';
 import {
@@ -94,6 +94,8 @@ let DEFAULT_HEIGHT: number = 900;
 
 // Timeout on restarting SDA in case it's stuck
 const LISTEN_TIMEOUT: number = 25 * 1000;
+
+cookieHandler.handleCookies();
 
 export class WindowHandler {
   /**
@@ -250,26 +252,6 @@ export class WindowHandler {
       'window-handler: createApplication mainWinPos: ' +
         JSON.stringify(this.config.mainWinPos),
     );
-
-    const cookies = session.defaultSession.cookies;
-    cookies.on('changed', async (_event, cookie, _cause, removed) => {
-      if (cookie.session && !removed) {
-        const url = `${cookie.secure ? 'https' : 'http'}://${cookie.domain}${
-          cookie.path
-        }`;
-        logger.info(`Persisting cookies for ${url}`);
-        await cookies.set({
-          url,
-          name: cookie.name,
-          value: cookie.value,
-          domain: cookie.domain,
-          path: cookie.path,
-          secure: cookie.secure,
-          httpOnly: cookie.httpOnly,
-          expirationDate: Math.floor(new Date().getTime() / 1000) + 1209600,
-        });
-      }
-    });
 
     let { isFullScreen, isMaximized } = this.config.mainWinPos
       ? this.config.mainWinPos
